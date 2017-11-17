323f2fbb76f39fb489af5fdd48e23ce870ef6233
Colin Woodbury <colingw@gmail.com>
2017-11-17 14:10:20 -0800

    Expose `--heroku` flag to bind to $PORT

--- src/Server.hs
+++ src/Server.hs
@@ -1,20 +1,29 @@
 {-# LANGUAGE DataKinds, TypeOperators #-}
+{-# LANGUAGE DeriveGeneric, DeriveAnyClass #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main ( main ) where
 
 import           Control.Monad.IO.Class (liftIO)
+import           Data.Bool (bool)
 import           Data.Proxy
 import qualified Data.Text as T
 import           Data.Word (Word32)
+import           GHC.Generics
 import qualified Network.Wai.Handler.Warp as W
+import           Options.Generic
 import           Servant.API
 import           Servant.Server
 import           Servant.Swagger
 import           Servant.Swagger.UI
+import           System.Environment (lookupEnv)
 import           Test.QuickCheck (Arbitrary, generate, arbitrary)
+import           Text.Read (readMaybe)
 import           Types
 
 ---
+
+data Env = Env { heroku :: Bool <?> "Bind to $PORT, as Heroku expects." } deriving (Generic, ParseRecord)
 
 type OSMesa = "users" :> Get '[JSON] [LightUser]
   :<|> "users"     :> "hashtag" :> Capture "tag" T.Text :> Get '[JSON] [LightUser]
@@ -43,4 +52,8 @@ hashtag :: T.Text -> Handler (Maybe Campaign)
 hashtag t = (\c -> Just $ c { tag = t }) <$> whatever
 
 main :: IO ()
-main = W.run 8081 app
+main = do
+  Env (Helpful h) <- getRecord "OSMesa Analytics Server"
+  port <- bool (pure 8081) f h
+  W.run port app
+  where f = maybe 8081 id . (>>= readMaybe) <$> lookupEnv "PORT"
